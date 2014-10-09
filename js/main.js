d7432c4245adeff36bbe938387cd6a3206a09a14
Marques Jordan <marques.jordan81@gmail.com>
2014-10-09 09:06:51 -0700

    Use Angular to make board and ng-click start

--- js/main.js
+++ js/main.js
@@ -12,4 +12,36 @@ myApp.controller("myController", function($scope){
 
 	$scope.boxes = [0,1,2,3,4,5,6,7,8];
 
+	/*$scope.boxes = {
+		boxnum: 0,
+		boxnum: 1,
+		boxnum: 2,
+		boxnum: 3,
+		boxnum: 4,
+		boxnum: 5,
+		boxnum: 6,
+		boxnum: 7,
+		boxnum: 8
+	};*/
+
+
+
+	$scope.changeBox = function(){
+		$scope.test = true;
+		// $scope.bg = "br"
+	}
+
+	$scope.row = function(){
+		if(($scope.$index % 3) == 0){
+			return true;
+		}
+	}
+
+	$scope.makeMove = function(num){
+		console.log(num);
+		$scope.myX = "X";
+	}
+
+
+
 });
