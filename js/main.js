9de9b12c31a65cd726e8674e1530660773057491
Marques Jordan <marques.jordan81@gmail.com>
2014-10-09 14:20:45 -0700

    Add player array with board indexed

--- js/main.js
+++ js/main.js
@@ -8,21 +8,27 @@ myApp.controller("myController", function($scope){
 		school: "General Assembly"
 	};
 
-	//$scope.boxes = [null,null,null,null,null,null,null,null,null];
 
-	$scope.boxes = [0,1,2,3,4,5,6,7,8];
+	//$scope.board = [null,null,null,null,null,null,null,null,null];
 
-	/*$scope.boxes = {
-		boxnum: 0,
-		boxnum: 1,
-		boxnum: 2,
-		boxnum: 3,
-		boxnum: 4,
-		boxnum: 5,
-		boxnum: 6,
-		boxnum: 7,
-		boxnum: 8
-	};*/
+	//$scope.boxes = [0,1,2,3,4,5,6,7,8];
+
+	$scope.boxes = [
+		{status: "Empty", boxNum: 0},
+		{status: "Empty", boxNum: 1},
+		{status: "Empty", boxNum: 2},
+		{status: "Empty", boxNum: 3},
+		{status: "Empty", boxNum: 4},
+		{status: "Empty", boxNum: 5},
+		{status: "Empty", boxNum: 6},
+		{status: "Empty", boxNum: 7},
+		{status: "Empty", boxNum: 8}
+	];
+
+	$scope.players = [
+		{name: "Player X", board: [], wins: 0},
+		{name: "Player Y", board: [], wins: 0}
+	];
 
 
 
@@ -41,6 +47,45 @@ myApp.controller("myController", function($scope){
 		console.log(num);
 		$scope.myX = "X";
 	}
+
+
+	$scope.movecounter = 0;
+
+	$scope.playerPicks = function(thisCell, num) {
+		if(thisCell.status != "Empty") return;
+	    $scope.movecounter = $scope.movecounter + 1 ;
+	    console.log("Cell was: " + thisCell.status) ;
+	    if (($scope.movecounter % 2) == 1) {
+	      thisCell.status = "X" ;
+	      console.log("The current index is " + num);
+	      $scope.players[0].board[num] = true;
+	      console.log($scope.players[0].board);
+	    } else {
+	      thisCell.status = "O" ;
+	    } 
+	    console.log("Cell is now: " + thisCell.status) ;
+	    console.log("Counter: " + $scope.movecounter);
+    };
+
+    $scope.checkWinner =function (user){
+
+            if( user[4] && ( (user[1] && user[7] ) 
+                || (user[3] && user[5]) 
+                    || ( user[0] && user[8] )
+                        || (user[2] && user[6]) ) ){
+                return true;
+            } 
+            else if( user[0] && ( ( user[1] && user[2] ) || ( user[3] && user[6] ) ) ){
+                return true;
+            }
+            else if( user[8] && ( ( user[2] && user[5] ) || ( user[6] && user[7] ) ) ){
+                return true;
+            }
+            else{
+                return false;
+            }
+
+        }
 
 
 
