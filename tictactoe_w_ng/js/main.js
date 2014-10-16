04bd8355d73d7a09307f44e9143905de80d64334
Marques Jordan <marques.jordan81@gmail.com>
2014-10-16 15:54:06 -0700

    Add files of TTT with ng only

---
+++ tictactoe_w_ng/js/main.js
@@ -0,0 +1,141 @@
+var myApp = angular.module("myApp", []);
+
+myApp.controller("myController", function($scope){
+
+	//Array of Objects to represnt TTT Board
+	//Status will change as user click box of indexed object
+	$scope.boxes = [
+		{status: "Empty"},
+		{status: "Empty"},
+		{status: "Empty"},
+		{status: "Empty"},
+		{status: "Empty"},
+		{status: "Empty"},
+		{status: "Empty"},
+		{status: "Empty"},
+		{status: "Empty"}
+	];
+
+	//Array of player objects:
+	//'board' is an array to keep track of player moves 
+	$scope.players = [
+		{name: "Joker", board: [], wins: 0, winner: 0},
+		{name: "Batman", board: [], wins: 0, winner: 1}
+	];
+
+
+	$scope.movecounter = 0; // Click Counter
+	$scope.endgame = false; // Trigger to end game
+	$scope.currentTurn = 1;
+	//$scope.turn = 1;
+	// On click send (board div and index)
+	$scope.playerPicks = function(thisCell, num) {
+		
+		// If box has been selected already
+		if(thisCell.status != "Empty") return; 
+	    
+	    $scope.movecounter = $scope.movecounter + 1 ;
+	    
+	    // $scope.turn = ($scope.movecounter % 2);
+	    //$scope.currentTurn = 0; 
+
+	    if ($scope.currentTurn == 1) {
+	      thisCell.status = "O" ;
+
+	      $scope.players[0].board[num] = true;
+		  $scope.endgame = $scope.checkWinner($scope.players[0].board);
+
+	      $scope.gameOver($scope.endgame, $scope.currentTurn);
+	      //$scope.currentTurn = 0;
+
+	    } else {
+	      thisCell.status = "X" ;
+
+	      $scope.players[1].board[num] = true;
+	      $scope.endgame = $scope.checkWinner($scope.players[1].board);
+
+	      $scope.gameOver($scope.endgame, $scope.currentTurn);
+	      //$scope.currentTurn = 1;
+	    }
+
+	    if($scope.movecounter == 9 && !$scope.endgame){
+	    	$scope.thewinner = "Cats Game"
+	    	$scope.newgame = true;
+	    }
+    };
+
+    $scope.gameOver = function(game, counter){
+	    if(game == true){
+			$scope.thewinner = $scope.players[counter].name + " is the winner";
+			$scope.players[counter].wins += 1;
+
+			$scope.currentTurn = $scope.players[counter].winner;
+
+			for($scope.i=0; $scope.i<9; $scope.i++){
+				if($scope.boxes[$scope.i].status == "Empty"){
+					$scope.boxes[$scope.i].status = "Fill";
+				}
+			}
+			$scope.newgame = true;
+			
+			//$scope.play = prompt("Do you want to play again?");
+		}else{
+			if($scope.currentTurn == 1){
+				$scope.currentTurn = 0;
+			}else{
+				$scope.currentTurn = 1;
+			}
+		}
+	};
+
+	$scope.restartGame = function(){
+
+		//Loop to Reset Board - and Player trackers
+		for($scope.p=0; $scope.p<9; $scope.p++){
+			$scope.boxes[$scope.p].status = "Empty";
+			$scope.players[0].board[$scope.p]=false;
+			$scope.players[1].board[$scope.p]=false;
+		}
+		$scope.movecounter = 0; // sets the click to 0
+		$scope.endgame = false; 
+		$scope.newgame = false;
+		//$scope.currentTurn = 0;
+		
+	}
+
+	$scope.resetGame = function(){
+
+		$scope.players[0].wins=0;
+		$scope.players[1].wins=0;
+		//Loop to Reset Board - and Player trackers
+		$scope.restartGame();
+	}
+
+    //passing in the player Object board array property into user
+    //Checks to see if there is a winner
+    $scope.checkWinner =function (user){
+
+            if( user[4] && ( (user[1] && user[7] ) 
+                || (user[3] && user[5]) 
+                    || ( user[0] && user[8] )
+                        || (user[2] && user[6]) ) ){
+            	//alert("Winner");
+                return true;
+            } 
+            else if( user[0] && ( ( user[1] && user[2] ) || ( user[3] && user[6] ) ) ){
+                //alert("Winner");
+                return true;
+            }
+            else if( user[8] && ( ( user[2] && user[5] ) || ( user[6] && user[7] ) ) ){
+                //alert("Winner");
+                return true;
+            }
+            else{
+                return false;
+            }
+
+        }
+
+
+
+});
